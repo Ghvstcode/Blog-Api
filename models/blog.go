2ab4c4b8e569ad391d8035115c00583becc7bc0c
Tobax <impoissants@gmail.com>
2020-07-23 20:17:56 +0100

    Save current State

--- models/blog.go
+++ models/blog.go
@@ -12,38 +12,37 @@ import (
 	l "github.com/GhvstCode/Blog-Api/utils/logger"
 )
 
-
 type BlogModel struct {
-	 ID        primitive.ObjectID `bson:"_id, omitempty" json:"id, omitempty"`
-	 Title     string 			  `bson:"title" json:"title, omitempty"`//Ensure title is not empty or greater than 150 characters
-	 Content   string 		      `bson:"content" json:"content, omitempty"`//Ensure content is not empty
-	 Author    string			   `bson:"author" json:"author, omitempty"`
-	 OwnerId     primitive.ObjectID `bson:"ownerId, omitempty" json:"ownerId, omitempty"`
-	 Published *bool					`bson:"published" json:"published, omitempty"`
-	 Paid      *bool					`bson:"Paid" json:"Paid, omitempty"`
-	 Price     int				`bson:"price" json:"price, omitempty"`
+	ID        primitive.ObjectID `bson:"_id, omitempty" json:"id, omitempty"`
+	Title     string             `bson:"title" json:"title, omitempty"`     //Ensure title is not empty or greater than 150 characters
+	Content   string             `bson:"content" json:"content, omitempty"` //Ensure content is not empty
+	Author    string             `bson:"author" json:"author, omitempty"`
+	OwnerId   primitive.ObjectID `bson:"ownerId, omitempty" json:"ownerId, omitempty"`
+	Published *bool              `bson:"published" json:"published, omitempty"`
+	Paid      *bool              `bson:"Paid" json:"Paid, omitempty"`
+	Price     int                `bson:"price" json:"price, omitempty"`
 }
 
 type ReBlogModel struct {
-	ID        string   			   `json:"id, omitempty"`
-	Title     string 			   `json:"title, omitempty"`
-	Content   string 		       `json:"content, omitempty"`
-	Author    string			   `json:"author, omitempty"`
-	OwnerId   string 				`json:"ownerId, omitempty"`
-	Published *bool					`json:"published, omitempty"`
-	Paid      *bool					`json:"Paid, omitempty"`
-	Price     int				`json:"price, omitempty"`
+	ID        string `json:"id, omitempty"`
+	Title     string `json:"title, omitempty"`
+	Content   string `json:"content, omitempty"`
+	Author    string `json:"author, omitempty"`
+	OwnerId   string `json:"ownerId, omitempty"`
+	Published *bool  `json:"published, omitempty"`
+	Paid      *bool  `json:"Paid, omitempty"`
+	Price     int    `json:"price, omitempty"`
 }
 
 type UpdateBlogModel struct {
-	Title     string 			   `json:"title, omitempty"`
-	Content   string 		       `json:"content, omitempty"`
-	Published *bool					`json:"published, omitempty"`
-	Paid      *bool					`json:"Paid, omitempty"`
-	Price     int				`json:"price, omitempty"`
+	Title     string `json:"title, omitempty"`
+	Content   string `json:"content, omitempty"`
+	Published *bool  `json:"published, omitempty"`
+	Paid      *bool  `json:"Paid, omitempty"`
+	Price     int    `json:"price, omitempty"`
 }
 
-func getID (id string) (primitive.ObjectID, error){
+func getID(id string) (primitive.ObjectID, error) {
 	postId, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
 		return postId, err
@@ -60,33 +59,34 @@ func Find(slice []string, val string) (int, bool) {
 	return -1, false
 }
 
-func Validate(b *BlogModel) *utils.Data{
+func Validate(b *BlogModel) *utils.Data {
 	if len(b.Title) >= 150 || len(b.Title) == 0 {
-		return utils.Response(false,"Title Must be greater than zero & less than 150 characters", http.StatusBadRequest)
+		return utils.Response(false, "Title Must be greater than zero & less than 150 characters", http.StatusBadRequest)
 	}
 
 	if len(b.Content) == 0 {
-		return utils.Response(false,"Content must not be empty", http.StatusBadRequest)
+		return utils.Response(false, "Content must not be empty", http.StatusBadRequest)
 	}
 
 	if len(b.Author) == 0 {
-		return utils.Response(false,"Please fill in the author field", http.StatusBadRequest)
+		return utils.Response(false, "Please fill in the author field", http.StatusBadRequest)
 	}
 
-	return utils.Response(true,"Validated", http.StatusOK)
+	return utils.Response(true, "Validated", http.StatusOK)
 }
 
-func (b *BlogModel)Create(Owner string) *utils.Data {
+func (b *BlogModel) Create(Owner string) *utils.Data {
 	//Check if a user already has a post with the title they are trying to create/update to avoid possible duplicates
 	resp := Validate(b)
-	ok := resp.Result; if !ok {
+	ok := resp.Result
+	if !ok {
 		return resp
 	}
 
-	Ownid, err := primitive.ObjectIDFromHex(Owner)
+	OwnId, err := primitive.ObjectIDFromHex(Owner)
 	if err != nil {
 		l.ErrorLogger.Println(err)
-		return utils.Response(false, "An Error occurred, Unable to Create Post" , http.StatusInternalServerError)
+		return utils.Response(false, "An Error occurred, Unable to Create Post", http.StatusInternalServerError)
 	}
 
 	res, err := Blog.InsertOne(context.TODO(), &BlogModel{
@@ -94,7 +94,7 @@ func (b *BlogModel)Create(Owner string) *utils.Data {
 		Title:     b.Title,
 		Content:   b.Content,
 		Author:    b.Author,
-		OwnerId:   Ownid,
+		OwnerId:   OwnId,
 		Published: b.Published,
 		Paid:      b.Paid,
 		Price:     b.Price,
@@ -126,42 +126,42 @@ func (b *BlogModel)Create(Owner string) *utils.Data {
 	return response
 }
 
-func (b *UpdateBlogModel) UpdatePost(id string) *utils.Data{
-//To-Do Validate Update.
+func (b *UpdateBlogModel) UpdatePost(id string) *utils.Data {
+	//To-Do Validate Update.
 	postId, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
 		l.ErrorLogger.Println(err)
-		return utils.Response(false, "An Error occurred, Unable to Create Post" , http.StatusInternalServerError)
+		return utils.Response(false, "An Error occurred, Unable to Create Post", http.StatusInternalServerError)
 	}
 
 	filter := bson.D{{"_id", postId}}
 	update := bson.M{"$set": b}
 	//opts := options.FindOneAndUpdate().new(true)
 
-	var bm  ReBlogModel
+	var bm ReBlogModel
 	var c BlogModel
 	err = Blog.FindOneAndUpdate(context.TODO(), filter, update).Decode(&bm)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
 			l.ErrorLogger.Print(err)
-			return utils.Response(false, "An Error occurred, Unable to Create Post" , http.StatusInternalServerError)
+			return utils.Response(false, "An Error occurred, Unable to Create Post", http.StatusInternalServerError)
 		}
 		l.ErrorLogger.Print(err)
-		return utils.Response(false, "An Error occurred, Unable to Create Post" , http.StatusInternalServerError)
+		return utils.Response(false, "An Error occurred, Unable to Create Post", http.StatusInternalServerError)
 	}
 
 	err = Blog.FindOne(context.TODO(), filter).Decode(&c)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
 			l.ErrorLogger.Print(err)
-			return utils.Response(false, "An Error occurred, Unable to Create Post" , http.StatusInternalServerError)
+			return utils.Response(false, "An Error occurred, Unable to Create Post", http.StatusInternalServerError)
 		}
 		l.ErrorLogger.Print(err)
-		return utils.Response(false, "An Error occurred, Unable to Create Post" , http.StatusInternalServerError)
+		return utils.Response(false, "An Error occurred, Unable to Create Post", http.StatusInternalServerError)
 	}
 
 	res := &ReBlogModel{
-		ID:       c.ID.Hex(),
+		ID:        c.ID.Hex(),
 		Title:     c.Title,
 		Content:   c.Content,
 		Author:    c.Author,
@@ -175,51 +175,50 @@ func (b *UpdateBlogModel) UpdatePost(id string) *utils.Data{
 	return response
 }
 
-func DeletePost(id string) *utils.Data{
+func DeletePost(id string) *utils.Data {
 	postId, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
 		l.ErrorLogger.Println(err)
-		return utils.Response(false, "An Error occurred, Unable to Delete Post" , http.StatusInternalServerError)
+		return utils.Response(false, "An Error occurred, Unable to Delete Post", http.StatusInternalServerError)
 	}
 
 	filter := bson.D{{"_id", postId}}
 	_, err = Blog.DeleteOne(context.TODO(), filter)
 	if err != nil {
 		l.ErrorLogger.Println(err)
-		return utils.Response(false, "An Error occurred, Unable to Delete Post" , http.StatusInternalServerError)
+		return utils.Response(false, "An Error occurred, Unable to Delete Post", http.StatusInternalServerError)
 	}
 
 	response := utils.Response(true, "Deleted", http.StatusOK)
 	return response
 }
 
-func GetPost(id string, UserID string)  *utils.Data{
+func GetPost(id string, UserID string) *utils.Data {
 	var c *UserModel
-	var b * BlogModel
+	var b *BlogModel
 	postId, err := getID(id)
 	if err != nil {
 		l.ErrorLogger.Println(err)
-		return utils.Response(false, "An Error occurred, Unable to Fetch Post" , http.StatusInternalServerError)
+		return utils.Response(false, "An Error occurred, Unable to Fetch Post", http.StatusInternalServerError)
 	}
 
 	userId, err := getID(UserID)
 	if err != nil {
 		l.ErrorLogger.Println(err)
-		return utils.Response(false, "An Error occurred, Unable to Fetch Post" , http.StatusInternalServerError)
+		return utils.Response(false, "An Error occurred, Unable to Fetch Post", http.StatusInternalServerError)
 	}
 
 	//filter := bson.D{{"_id", postId}}
 	err = Blog.FindOne(context.TODO(), bson.D{{"_id", postId}}).Decode(&b)
 	if err != nil {
 		l.ErrorLogger.Print(err)
-		return utils.Response(false, "An Error occurred, Unable to Fetch Post" , http.StatusInternalServerError)
+		return utils.Response(false, "An Error occurred, Unable to Fetch Post", http.StatusInternalServerError)
 	}
-
 
 	err = User.FindOne(context.TODO(), bson.D{{"_id", userId}}).Decode(&c)
 	if err != nil {
 		l.ErrorLogger.Print(err)
-		return utils.Response(false, "An Error occurred, Unable to Fetch Post" , http.StatusInternalServerError)
+		return utils.Response(false, "An Error occurred, Unable to Fetch Post", http.StatusInternalServerError)
 	}
 	//fmt.Print(b.OwnerId)
 	//fmt.Print(userId)
@@ -230,11 +229,11 @@ func GetPost(id string, UserID string)  *utils.Data{
 	//if !*b.Published && b.OwnerId == userId || !returnBool{
 	//	return utils.Response(false, "Post Not found" , http.StatusNotFound)
 	//}
-	if b.Published == nil || !(*b.Published) && !returnBool || !(b.OwnerId == userId){
-		return utils.Response(false, "Post Not found" , http.StatusNotFound)
+	if b.Published == nil || !(*b.Published) && !returnBool || !(b.OwnerId == userId) {
+		return utils.Response(false, "Post Not found", http.StatusNotFound)
 	}
 	res := &ReBlogModel{
-		ID:       b.ID.Hex(),
+		ID:        b.ID.Hex(),
 		Title:     b.Title,
 		Content:   b.Content,
 		Author:    b.Author,
